9c14666c31742c30988aed3400478581fa558c77
Kat <kathybuggs@gmail.com>
2017-11-09 04:14:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,7 @@ var playlist = { artistName: "songTitle"}
 
 // objects updatePlaylist(playlist, artistName, songTitle)
 // adds the 'artistName: songTitle' key-value pair to 'playist'
-
 function updatePlaylist(object, artistName, songTitle){
-  object[artistName] = songTitle
+  object[artistName] = songTitle // original code * object.artistName = songTitle * did not work
   return object
-}
+} 
