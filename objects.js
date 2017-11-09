bdff6096591bc867babb239fbc59d8d1fb3a4cef
Kat <kathybuggs@gmail.com>
2017-11-09 04:10:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,10 @@
 // keys will be artist names and the values will be song titles.
 var playlist = { artistName: "songTitle"}
 
+// objects updatePlaylist(playlist, artistName, songTitle) 
+// adds the 'artistName: songTitle' key-value pair to 'playist'
+
+function updatePlaylist(object, artistName, songTitle){
+  object.artistName = songTitle
+  return object
+}
